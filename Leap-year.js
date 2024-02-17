5810a9c5a180b96ebbae1322c5c4ed77aa8d12f6
yeasinshakil <shakilrock1998@gmail.com>
2024-02-17 23:23:56 +0600

    loop-year

---
+++ Leap-year.js
@@ -0,0 +1,11 @@
+const isLeapYear = year => {
+    if (year <= 0) {
+        return false;
+    }
+
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
